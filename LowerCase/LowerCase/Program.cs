3fc3177559d0b3428e218d2ec3c07e52187e293a
laurasjea <laurazdon@ou.edu>
2016-08-03 08:07:13 -0500

    Lower Case

---
+++ LowerCase/LowerCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                List<string> lowercase = new List<string>();
+
+                for (int i = 0; i< args.Length; i++)
+                {
+                    lowercase.Add(line.ToString().ToLower());
+                }
+
+                lowercase.ForEach(Console.WriteLine);
+
+            }
+    }
+}
