8bba1aac999929b106ab7bef7bb5edb2658ecd85
laurasjea <laurazdon@ou.edu>
2016-08-03 08:13:11 -0500

    Word Count

---
+++ Word Count/Word Count/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Word_Count
+{
+    class Phrase
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Your sentence:");
+            string phrase = Console.ReadLine();
+
+            WordCount(phrase);
+        }
+
+        public static IDictionary<string, int> WordCount(string phrase)
+        {
+            var match = new Dictionary<string, int>();
+            string[] words = phrase.Split(new char[] { ' ', ',', ';', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+        }  
+    }
+}
+
+
+
+
+
+
+
+
+
