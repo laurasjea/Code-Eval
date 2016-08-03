613e46d8cff729c8e7f4c5667f9597d4f8467a00
laurasjea <laurazdon@ou.edu>
2016-08-03 07:58:44 -0500

    Beautiful Strings

---
+++ BeautifulStrings/BeautifulStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulStrings
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            List<string> letters = new List<string>();
+            string[] array = line.Split(' ');
+            CountingLetters(array, letters);
+        }
+
+        public static void CountingLetters(string[] array, List<string> letters)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].All(char.IsLetter))
+                {
+                    char[] charArray = array[i].ToCharArray();
+                    CountLetters();
+                }
+                
+                
+            }
+        }
+
+        public static void CountLetters(charArra)
+    }
+}
