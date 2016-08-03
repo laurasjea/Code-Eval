2f03853b61f2174f04fcb584a4bf2e8cf76cc505
laurasjea <laurazdon@ou.edu>
2016-08-03 08:10:54 -0500

    Reverse Statement

---
+++ ReverseStatement/ReverseStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Statement:");
+            string statement = Console.ReadLine();
+            string[] array = statement.Split(' ');
+
+            Console.WriteLine("Reverse is:");
+            Array.Reverse(array);
+            Console.WriteLine(string.Join(" ", array));
+
+            Console.ReadKey(true);
+        }
+
+    }
+}
