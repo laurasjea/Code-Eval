43ed4e6a0b4c21380a806372fd0a15e32356f1e3
laurasjea <laurazdon@ou.edu>
2016-08-03 08:10:38 -0500

    Reverse Words

---
+++ Reverse Words/Reverse Words/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+                string[] array = line.Split(' ');
+                string newLine = "";
+
+                for (int k = (array.Length - 1); k >= 0; k--)
+                {
+                    newLine += array[k] + " ";
+                }
+
+                Array.Clear(array, 0, array.Length);
+                list.Add(newLine);
+        }
+        list.ForEach(Console.WriteLine);
+    }
+}
