aa28130d6942cd3a25fd511b55449b215f9a5633
laurasjea <laurazdon@ou.edu>
2016-08-03 08:09:53 -0500

    Print Rectangle

---
+++ PrintRectangle/PrintRectangle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintRectangle
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Length:");
+                string lengthString = Console.ReadLine();
+                int lengthInteger = Convert.ToInt32(lengthString);
+                Console.WriteLine("Width:");
+                string widthString = Console.ReadLine();
+                int widthInteger = Convert.ToInt32(widthString);
+
+                string[] rectangle = Rectangle(lengthInteger, widthInteger);
+
+                foreach (var i in rectangle)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string[] Rectangle(int lengthInteger, int widthInteger)
+        {
+            List<string> array = new List<string>();
+
+            string top = new String('*', lengthInteger);
+            array.Add(top);
+
+            for (int i=widthInteger; i > 2; i--)
+            {
+                string middle = new String(' ', lengthInteger - 2);
+                string middleRows = "*" + middle + "*";
+                array.Add(middleRows);
+                widthInteger -= 1;
+            }
+
+            string bottom = new String('*', lengthInteger);
+            array.Add(bottom);
+
+            string[] rectangle = array.ToArray();
+
+            return rectangle;
+        }
+    }
+}
---
+++ PrintRectangle/PrintRectangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrintRectangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrintRectangle")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("73b41a0f-0649-4d06-b1f5-e8f87a8c32bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
