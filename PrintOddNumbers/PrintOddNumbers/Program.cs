526eafdc754994ed98761e29563800cc2468191f
laurasjea <laurazdon@ou.edu>
2016-08-03 08:09:39 -0500

    Print Odd Numbers

---
+++ PrintOddNumbers/PrintOddNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> oddNumbers = new List<string>();
+        for (int k = 0; k < 100; k++)
+        {
+            if (k % 2 != 0)
+            {
+                oddNumbers.Add(k.ToString());
+            }
+        }
+        oddNumbers.ForEach(Console.WriteLine);
+    }
+
+}
