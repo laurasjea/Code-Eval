579ac2f6cd5232319f836103921f5f088384418b
laurasjea <laurazdon@ou.edu>
2016-08-03 08:08:28 -0500

    Next To Last String

---
+++ NextToLastString/NextToLastString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> words = new List<string>();
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+                string[] array = line.Split(' ');
+
+                words.Add((array[(array.Length - 2)]));
+
+        }
+
+        words.ForEach(Console.WriteLine);
+    }
+}
